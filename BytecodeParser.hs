02a404787bdce9249b2c5be888676caa264e4d41
Joni Freeman <joni.freeman@reaktor.fi>
2009-12-19 23:10:08 +0200

    rename primitive readers

--- BytecodeParser.hs
+++ BytecodeParser.hs
@@ -46,9 +46,10 @@ parse bs = Class "com.example.Foo" "java.lang.Object" [] [] []
 
 skipHeader :: L.ByteString -> L.ByteString
 skipHeader = L8.drop 8
+skipAccessFlags = L8.drop 2
 
 readConstantPoolCount :: L.ByteString -> (Int, L.ByteString)
-readConstantPoolCount = getNum2
+readConstantPoolCount = getNum16
 
 readConstantPoolEntries :: Int -> L.ByteString -> ([CPEntry], L.ByteString)
 readConstantPoolEntries 0 bs = ([], bs)
@@ -58,32 +59,32 @@ readConstantPoolEntries x bs = let (e, rem1) = readConstantPoolEntry bs
 
 -- FIXME cleanup this ugly implementation, how to chain (x, rem) ?
 readConstantPoolEntry :: L.ByteString -> (CPEntry, L.ByteString)
-readConstantPoolEntry bs = let tag = getNum1 bs
+readConstantPoolEntry bs = let tag = getNum8 bs
                                e1 entry (idx, bs) = (entry idx, bs)
                                e2 entry (idx1, bs) f = let (idx2, rem) = f bs
                                                         in (entry idx1 idx2, rem)
                            in case fst tag of
-                                7  -> e1 Classref (getNum2 $ snd tag)
-                                9  -> e2 Fieldref (getNum2 $ snd tag) getNum2
-                                10 -> e2 Methodref (getNum2 $ snd tag) getNum2
-                                11 -> e2 InterfaceMethodref (getNum2 $ snd tag) getNum2
-                                12 -> e2 NameAndType (getNum2 $ snd tag) getNum2
-                                8  -> e1 Stringref (getNum2 $ snd tag)
+                                7  -> e1 Classref (getNum16 $ snd tag)
+                                9  -> e2 Fieldref (getNum16 $ snd tag) getNum16
+                                10 -> e2 Methodref (getNum16 $ snd tag) getNum16
+                                11 -> e2 InterfaceMethodref (getNum16 $ snd tag) getNum16
+                                12 -> e2 NameAndType (getNum16 $ snd tag) getNum16
+                                8  -> e1 Stringref (getNum16 $ snd tag)
                                 3  -> e1 Other (getInt $ snd tag)
                                 4  -> e1 Other (getInt $ snd tag)
                                 5  -> e2 Other2 (getInt $ snd tag) getInt
                                 6  -> e2 Other2 (getInt $ snd tag) getInt
                                 1  -> e1 Utf8 (getUtf8 $ snd tag)
 
-getNum1 :: L.ByteString -> (Int, L.ByteString)
-getNum1 bs = (fromIntegral $ L.head bs, L.tail bs)
+getNum8 :: L.ByteString -> (Int, L.ByteString)
+getNum8 bs = (fromIntegral $ L.head bs, L.tail bs)
 
-getNum2 :: L.ByteString -> (Int, L.ByteString)
-getNum2 bs = case L.unpack bs of
-              x : y : rest -> ((fromIntegral x) * 16 + fromIntegral y, L.drop 2 bs)
+getNum16 :: L.ByteString -> (Int, L.ByteString)
+getNum16 bs = case L.unpack bs of
+                x : y : rest -> ((fromIntegral x) * 16 + fromIntegral y, L.drop 2 bs)
 
 getUtf8 :: L.ByteString -> (String, L.ByteString)
-getUtf8 bs = let (length, rem) = getNum1 bs
+getUtf8 bs = let (length, rem) = getNum16 bs
                  n = fromIntegral length
              in (U8.toString $ L.take n rem, L.drop n rem)
 
@@ -91,8 +92,11 @@ getInt :: L.ByteString -> (Int, L.ByteString)
 getInt = undefined
 
 -- FIXME remove, just to test stuff
-foo bs = let (count, rem) = readConstantPoolCount $ skipHeader bs
-         in readConstantPoolEntries (count-1) rem
+foo bs = let (count, rem1) = readConstantPoolCount $ skipHeader bs
+             (cp, rem2) = readConstantPoolEntries (count-1) rem1
+             (fqn, rem3) = getNum16 $ skipAccessFlags rem2
+         in (fqn, cp)
+
 main = test
 test = do
   inh <- openBinaryFile "Test.class" ReadMode
