53d3fc1cf979bc4b690af93e8c7d48bc96015dc8
Joni Freeman <joni.freeman@reaktor.fi>
2009-12-19 02:04:02 +0200

    parse constant pool size

--- BytecodeParser.hs
+++ BytecodeParser.hs
@@ -1,21 +1,36 @@
 import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.ByteString.Lazy as L
 import System.IO -- FIXME can be removed 
+import Debug.Trace -- FIXME can be removed 
 
 
 type Method = String
 
-data CallGraph = CallGraph {
+--data CallGraph = CallGraph {
+--      method :: Method
+--    , callsites :: [CallGraph]
+--    } deriving (Show)
+
+data Invocations = Invocations {
       method :: Method
-    , callsites :: [CallGraph]
+    , callsites :: [Method]
     } deriving (Show)
 
 -- http://www.murrayc.com/learning/java/java_classfileformat.shtml
-parse :: L.ByteString -> [CallGraph]
-parse = []
+parse :: L.ByteString -> [Invocations]
+parse bs = []
+
+foo bs = constantPoolCount $ skipHeader bs
 
 skipHeader :: L.ByteString -> L.ByteString
 skipHeader = L8.drop 8
+
+constantPoolCount :: L.ByteString -> (Int, L.ByteString)
+constantPoolCount bs = getNum2 bs
+
+getNum2 :: L.ByteString -> (Int, L.ByteString)
+getNum2 bs = case L.unpack bs of
+              x : y : rest -> ((fromIntegral x) * 16 + fromIntegral y, L.drop 2 bs)
 
 
 -- FIXME remove, just for testing stuff
@@ -24,4 +39,6 @@ test = do
   inh <- openBinaryFile "Test.class" ReadMode
   graph <- L.hGetContents inh >>= \bs -> return (parse bs)
   putStrLn $ show graph
+  x <- L.hGetContents inh >>= \bs -> return (foo bs)
+  putStrLn $ show x
   hClose inh
