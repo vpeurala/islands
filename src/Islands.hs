4f35215f51e19a0870a180e0b965cb8573422fae
Joni Freeman <joni.freeman@reaktor.fi>
2010-01-01 13:43:20 +0200

    Parse real configuration file.
    Signed-off-by: Joni Freeman <joni.freeman@reaktor.fi>
    Signed-off-by: Ville Peurala <ville.peurala@gmail.com>

--- src/Islands.hs
+++ src/Islands.hs
@@ -9,15 +9,16 @@ data Config = Config {
       } deriving (Show)
 
 main = do doesConfigFileExist <- doesFileExist ".islands"
-          putStrLn $ show doesConfigFileExist
+          configFile <- readFile ".islands"
+          putStrLn $ show $ parse configFile
 
 --parse :: String -> Map String [String]
 parse input = mkConfig $ Map.fromList $ map (mkConfigLine . splitLine) (lines input)
-    where parseValues = splitRegex (mkRegex ",")
+    where parseValues = splitRegex (mkRegex "\\s*,\\s*")
           mkConfigLine (k, v) = (k, parseValues v)
           mkConfig :: Map String [String] -> Config 
           mkConfig m = Config (m ! "classpath_roots") (m ! "execution_roots")
 
 splitLine :: String -> (String, String)
-splitLine l = let (v1 : v2 : []) = splitRegex (mkRegex "=") l
+splitLine l = let (v1 : v2 : []) = splitRegex (mkRegex "\\s*=\\s*") l
             in (v1, v2)
