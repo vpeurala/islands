30740a8bd287e8baf982a4d0b41603c8d2f33023
Joni Freeman <joni.freeman@reaktor.fi>
2010-01-01 13:09:03 +0200

    Parse each line.

--- src/Islands.hs
+++ src/Islands.hs
@@ -1,4 +1,21 @@
+import qualified Data.Map as Map
 import System.Directory
+import Text.Regex
+
+data Config = Config {
+      classpathRoots :: [FilePath]
+    , executionRoots :: [FilePath]                        
+      } deriving (Show)
 
 main = do doesConfigFileExist <- doesFileExist ".islands"
           putStrLn $ show doesConfigFileExist
+
+--parse :: String -> Map String [String]
+parse input = Map.fromList $ map (\l -> mkConfig $ splitLine l) (lines input)
+    where parseValues = splitRegex (mkRegex ",")
+          mkConfig (k, v) = (k, parseValues v)
+         
+
+splitLine :: String -> (String, String)
+splitLine l = let (v1 : v2 : []) = splitRegex (mkRegex "=") l
+            in (v1, v2)
